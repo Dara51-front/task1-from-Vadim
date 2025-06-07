3a53b5f59bc247ed42dff2f97d896b0387d65506
Darina <darinakrasilnikova1@gmail.com>
2025-06-07 21:36:54 +0300

    maked dinamic table

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,14 +1,40 @@
-import { data } from './data' //  используем начальные данные
+import { data } from './data/index.js' //  используем начальные данные
 
 import './Table.css'
 
 function Table() {
+	function handleClick(e) {
+		console.log(e)
+	}
+
 	return (
 		<>
 			<h1>Table</h1>
-			{/* делаем динамический рендер таблицы */}
+			<table>
+			<thead>
+				<tr>
+					<th>Имя</th>
+					<th>Возраст</th>
+					<th>Удалить</th>
+				</tr>
+			</thead>
+			<tbody>
+			{ data.map(elem => (
+				<tr key={ elem.id }>
+					<td>{ elem.name }</td>
+					<td>{ elem.age}</td>
+					<td>
+						<button 
+							id={ elem.id }
+							onClick={() => handleClick(elem) }
+						>Удалить</button>
+					</td>
+				</tr>
+		)) }
+			</tbody>	
+			</table>
 		</>
-	)
+	) 
 }
 
 export default Table
